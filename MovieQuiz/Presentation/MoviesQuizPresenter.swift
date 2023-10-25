12fe127689d7f05bf86247636ba0c8eff899e576
Khamin Gleb <nimik19@yandex.ru>
2023-10-25 23:08:13 +0300

    Перед последним уроком

---
+++ MovieQuiz/Presentation/MoviesQuizPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesQuizPresenter.swift
+//  MovieQuiz
+//
+//  Created by Глеб Хамин on 25.10.2023.
+//
+
+import Foundation
---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Глеб Хамин on 22.10.2023.
+//
+
+import Foundation
